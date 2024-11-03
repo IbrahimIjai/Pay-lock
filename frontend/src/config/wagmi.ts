830347be7ceedebe2d8ce8b4fedec9e25fd57b3a
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2024-11-03 02:37:30 +0100

    fixed build error

--- frontend/src/config/wagmi.ts
+++ frontend/src/config/wagmi.ts
@@ -13,8 +13,6 @@ export const networks = [sepolia];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
-	//@ts-expect-error: unknown types
-
 	storage: createStorage({
 		storage: cookieStorage,
 	}),
